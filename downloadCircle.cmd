c3d3d8a6661541547acd731b6a1ab3047dc3a242
anbran223 <123478828+anbran223@users.noreply.github.com>
2023-03-05 12:06:24 +0800

    Create downloadCircle.cmd

---
+++ downloadCircle.cmd
@@ -0,0 +1,4 @@
+@echo off
+set url=https://github.com/anbran223/3-Packs-of-Brandon-s-Extension/blob/main/com.brandon.circle.aix?raw=true
+set filename=file.txt
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%https://github.com/anbran223/3-Packs-of-Brandon-s-Extension/blob/main/com.brandon.circle.aix?raw=true%','%com.brandon.circle.aix%')"
