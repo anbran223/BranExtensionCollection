a3855d3ed452bf09868dbe846417ce246e2b255f
anbran223 <123478828+anbran223@users.noreply.github.com>
2023-03-05 12:20:52 +0800

    Create downloadTextToHTML.cmd

---
+++ downloadTextToHTML.cmd
@@ -0,0 +1,4 @@
+@echo off
+set url=https://github.com/anbran223/3-Packs-of-Brandon-s-Extension/blob/main/com.brandon.texttohtml.aix?raw=true
+set filename=com.brandon.texttohtml.aix
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%https://github.com/anbran223/3-Packs-of-Brandon-s-Extension/blob/main/com.brandon.texttohtml.aix?raw=true%','%com.brandon.texttohtml.aix%')"
