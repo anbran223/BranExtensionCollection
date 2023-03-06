9a92cdc702e093ba9dff972fc068343c38b99ef3
anbran223 <123478828+anbran223@users.noreply.github.com>
2023-03-06 18:39:03 +0800

    Delete downloadSearching.cmd

--- downloadSearching.cmd
+++
@@ -1,5 +0,0 @@
-
-@echo off
-set url=https://github.com/anbran223/3-Packs-of-Brandon-s-Extension/blob/main/com.brandon.searching.aix?raw=true
-set filename=com.brandon.searching.aix
-powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%https://github.com/anbran223/3-Packs-of-Brandon-s-Extension/blob/main/com.brandon.searching.aix?raw=true%','%com.brandon.searching.aix%')"
