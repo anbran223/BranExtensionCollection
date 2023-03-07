2eac62c4fe3993d06e6f75f92a0c69acfdcf8dbd
anbran223 <123478828+anbran223@users.noreply.github.com>
2023-03-07 09:32:08 +0800

    Delete downloadWeWe.cmd

--- downloadWeWe.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-set url=https://github.com/anbran223/3-Packs-of-Brandon-s-Extension/blob/main/com.brandon.wewedictionary.aix?raw=true
-set filename=com.brandon.wewedictionary.aix
-powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%https://github.com/anbran223/3-Packs-of-Brandon-s-Extension/blob/main/com.brandon.wewedictionary.aix?raw=true%','%com.brandon.wewedictionary.aix%')"
